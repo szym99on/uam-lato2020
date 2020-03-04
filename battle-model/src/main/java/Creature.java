1570ba498865363d0fbd0b05d9f4388783eba879
software-wizard <przemek@software-wizard.pl>
2020-03-04 11:16:14 +0100

    first implementation

--- battle-model/src/main/java/Creature.java
+++ battle-model/src/main/java/Creature.java
@@ -1,8 +1,20 @@
 public class Creature {
+
+    private final int maxHp;
+    private final int attack;
+    private int currentHp;
+
+    public Creature(int aMaxHp, int aAttack) {
+        maxHp = aMaxHp;
+        attack = aAttack;
+        currentHp = maxHp;
+    }
+
     public void attack(Creature c1) {
+        c1.currentHp = c1.currentHp - attack;
     }
 
     public int getHp() {
-        return 8;
+        return currentHp;
     }
 }
--- battle-model/src/test/java/CreatureTest.java
+++ battle-model/src/test/java/CreatureTest.java
@@ -4,14 +4,16 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CreatureTest {
 
+    private final static int NOT_MPORTANT = 1;
+
     @Test
     void creatureOneShouldLostTwoHp(){
-        Creature c1 = new Creature();
-        Creature c2 = new Creature();
+        Creature c1 = new Creature(10,NOT_MPORTANT);
+        Creature c2 = new Creature(NOT_MPORTANT,2);
 
         c2.attack(c1);
 
-        assertEquals(8,c2.getHp());
+        assertEquals(8,c1.getHp());
     }
 
 }
