4860bbc63569d3d361b9ed0ae6ddb06763f7d606
software-wizard <przemek@software-wizard.pl>
2020-04-18 11:28:02 +0200

    exception example

--- battle-model/src/main/java/Board.java
+++ battle-model/src/main/java/Board.java
@@ -5,12 +5,18 @@ import java.util.Map;
 public class Board {
 
     private final Map<Point, Creature> board;
+    private final static int BOARD_WIDTH = 14;
+    private final static int BOARD_HIGH = 9;
 
     public Board() {
         board = new HashMap<>();
     }
 
     public void putCreature(int x, int y, Creature aCreature) {
+        if (x > BOARD_WIDTH || x < 0 || y > BOARD_HIGH || y < 0){
+            throw new IllegalArgumentException("You tried put creature outside the board");
+        }
+
         board.put(new Point(x,y), aCreature);
     }
 
