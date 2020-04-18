f93e66533a31177bdf370bf1e7327ca3c28d6a09
software-wizard <przemek@software-wizard.pl>
2020-04-18 11:29:54 +0200

    test exception

--- battle-model/src/test/java/BoardTest.java
+++ battle-model/src/test/java/BoardTest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -15,5 +16,13 @@ class BoardTest {
 
         assertEquals(c1,board.getCreature(1,1));
         assertEquals(c2,board.getCreature(2,2));
+    }
+
+    @Test
+    void shouldThrowExceptionWhileYouTryAddCreatureOutsideTheMap(){
+        Board board = new Board();
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            board.putCreature(1,100, Creature.builder().aName("C1").build());
+        });
     }
 }
