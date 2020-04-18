f37b0f5b071337c2198af17669d18a48050d0266
software-wizard <przemek@software-wizard.pl>
2020-04-18 09:16:01 +0200

    add armor statistic - test

--- battle-model/src/main/java/Creature.java
+++ battle-model/src/main/java/Creature.java
@@ -2,16 +2,18 @@ public class Creature {
 
     private final int maxHp;
     private final int attack;
+    private final int armor;
     private int currentHp;
 
-    public Creature(int aMaxHp, int aAttack) {
+    public Creature(int aMaxHp, int aAttack, int aArmor) {
         maxHp = aMaxHp;
         attack = aAttack;
         currentHp = maxHp;
+        armor = aArmor;
     }
 
-    public void attack(Creature c1) {
-        c1.currentHp = c1.currentHp - attack;
+    public void attack(Creature aDefender) {
+        aDefender.currentHp = aDefender.currentHp - attack;
     }
 
     public int getHp() {
--- battle-model/src/test/java/CreatureTest.java
+++ battle-model/src/test/java/CreatureTest.java
@@ -4,16 +4,28 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CreatureTest {
 
-    private final static int NOT_MPORTANT = 1;
+    private final static int NOT_IMPORTANT_1 = 1;
+    private final static int NOT_IMPORTANT_0 = 0;
 
     @Test
     void creatureOneShouldLostTwoHp(){
-        Creature c1 = new Creature(10,NOT_MPORTANT);
-        Creature c2 = new Creature(NOT_MPORTANT,2);
+        Creature c1 = new Creature(10, NOT_IMPORTANT_1, NOT_IMPORTANT_0);
+        Creature c2 = new Creature(NOT_IMPORTANT_0,2, NOT_IMPORTANT_0);
 
         c2.attack(c1);
 
         assertEquals(8,c1.getHp());
     }
+
+    @Test
+    void creatureShouldLostOneHpBecauseHasOneDefence(){
+        Creature defender = new Creature(10, NOT_IMPORTANT_1,1);
+        Creature attacker = new Creature(NOT_IMPORTANT_1,2, NOT_IMPORTANT_1);
+
+        attacker.attack(defender);
+
+        assertEquals(9,defender.getHp());
+    }
+
 
 }
