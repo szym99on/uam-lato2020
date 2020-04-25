b0f02a74a0426e22c0ad45976bd3322f94cbdcee
software-wizard <przemek@software-wizard.pl>
2020-04-25 13:33:50 +0200

    ignore armor implementation

--- battle-model/src/main/java/pl/psi/game/fractions/DealDamageCounterWithIgnoreArmorStrategy.java
+++ battle-model/src/main/java/pl/psi/game/fractions/DealDamageCounterWithIgnoreArmorStrategy.java
@@ -1,8 +1,24 @@
 package pl.psi.game.fractions;
 
+import java.util.Random;
+
 public class DealDamageCounterWithIgnoreArmorStrategy implements DealDamageCounterStrategyIf{
+
+    private final static Random random = new Random();
+
     @Override
     public int countDamageToDeal(Creature aAttacker, Creature aDefender) {
-        return 0;
+        int rand = random.nextInt(aAttacker.getAttack().upperEndpoint() - aAttacker.getAttack().lowerEndpoint() + 1);
+        int damageToDeal = aAttacker.getAttack().lowerEndpoint()+rand;
+
+        if (damageToDeal <= 0) {
+            damageToDeal = 1;
+        }
+
+        return damageToDeal;
     }
+
+
+
+
 }
