4e8b51e71a1915a3177d7945ef1370a6fb3eaa38
Jakub Przybyla <kubaprzybya@gmail.com>
2020-05-06 18:37:37 +0200

    add first TestRequest in Spellbook

--- battle-model/src/test/java/pl/psi/game/spellbook/ArtifactsRequest.java
+++ battle-model/src/test/java/pl/psi/game/spellbook/ArtifactsRequest.java
@@ -1,4 +1,15 @@
 package pl.psi.game.spellbook;
 
+import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class ArtifactsRequest {
+
+    @Test
+    void shouldReturnSpellDuration(){
+        SpellInfo spell = SpellBookInfoFactory.getSpell(SpellBookInfoFactory.AIR_SHIELD);
+        assertEquals("Air shield", spell.getName());
+        assertEquals(1, spell.getDuration());
+    }
+    
 }
