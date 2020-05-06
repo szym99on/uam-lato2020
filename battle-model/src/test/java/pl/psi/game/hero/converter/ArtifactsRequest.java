04952ab3fc1b684b482bdc166473552c038c2a02
Jakub Przybyla <kubaprzybya@gmail.com>
2020-05-06 19:04:34 +0200

    add first test request in Converter

--- battle-model/src/test/java/pl/psi/game/hero/converter/ArtifactsRequest.java
+++ battle-model/src/test/java/pl/psi/game/hero/converter/ArtifactsRequest.java
@@ -1,4 +1,26 @@
-package pl.psi.game.hero.converter;
-
-public class ArtifactsRequest {
-}
+//package pl.psi.game.hero.converter;
+//
+//import org.junit.jupiter.api.Disabled;
+//import org.junit.jupiter.api.Test;
+//import pl.psi.game.fractions.Creature;
+//import pl.psi.game.hero.artifacts.Artifact;
+//import pl.psi.game.hero.artifacts.ArtifactsInfoFactory;
+//import pl.psi.game.spellbook.SpellBookInfoFactory;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//public class ArtifactsRequest {
+//    @Test
+//    void shouldReturnHerosPrimarySkills(){
+//        Hero hero = Hero.builder().aAttack(3).aDefence(4).aSpellPower(2).aKnowledge(5).build();
+//
+//        assertEquals(3, hero.getAttack());
+//        assertEquals(4, hero.getDefence());
+//        assertEquals(2, hero.getSpellPower());
+//        assertEquals(5, hero.getKnowledge());
+//    }
+//
+//}
