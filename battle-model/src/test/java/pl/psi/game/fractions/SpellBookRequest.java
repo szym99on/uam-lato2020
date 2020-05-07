35b854757352e556b09e86b38295a580325d9bd3
Mateusz Wawrzonek <matwaw4@st.amu.edu.pl>
2020-05-07 23:16:38 +0200

    Add BuffDebuf TestRequest

--- battle-model/src/test/java/pl/psi/game/fractions/SpellBookRequest.java
+++ battle-model/src/test/java/pl/psi/game/fractions/SpellBookRequest.java
@@ -1,4 +1,28 @@
 package pl.psi.game.fractions;
 
+import org.junit.jupiter.api.Test;
+import pl.psi.game.spellbook.Spell;
+
+import java.util.Collection;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class SpellBookRequest {
+/*
+    @Test
+    void creatureShouldReturnActiveEffects(){
+        Collection<Spell> activeEffects = Creature.getActiveEffects();
+
+    }
+
+    @Test
+    void creatureShouldAcceptSpell(){
+        Creature creature = Creature.builder().build();
+        Spell spell = Spell.builder().build();
+
+        Creature.addEffect(spell);
+
+    }
+*/
+
 }
--- battle-model/src/test/java/pl/psi/game/hero/SpellBookRequest.java
+++ battle-model/src/test/java/pl/psi/game/hero/SpellBookRequest.java
@@ -1,4 +1,23 @@
 package pl.psi.game.hero;
 
+import org.junit.jupiter.api.Test;
+import pl.psi.game.fractions.Creature;
+import pl.psi.game.hero.converter.Hero;
+import pl.psi.game.spellbook.Spell;
+
+import java.util.List;
+
 public class SpellBookRequest {
+
+ /*   @Test
+    void shouldBuffAllAlliedTroops(){
+
+        Hero activeHero = ...;
+        List<Creature> creatures = activeHero.getCreatures();
+        Spell spell = Spell.builder().build();
+
+        creatures.forEach(a -> spell.cast(a));
+
+
+    }*/
 }
