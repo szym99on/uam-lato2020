f8c0a2846f21b04b4e4b3fa72f1daf78ee8a4e61
Szymon <szym99on@gmail.com>
2020-05-08 01:07:30 +0200

    Short test for damage spells

--- battle-model/src/test/java/pl/psi/game/spellbook/DamageSpellsTest.java
+++ battle-model/src/test/java/pl/psi/game/spellbook/DamageSpellsTest.java
@@ -1,12 +1,23 @@
 package pl.psi.game.spellbook;
 
+
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import pl.psi.game.fractions.Creature;
-import pl.psi.game.fractions.NecropolisCreatureFactory;
 
 public class DamageSpellsTest {
-    @Test
-    void test(){
-    }
 
+    @Test
+    @Disabled
+    void shouldDealDamageToCreature() {
+        Creature creature = Creature.builder().aMaxHp(10).aArmor(100).build();
+
+        SpellInfo spellInfo = SpellBookInfoFactory.getSpell("Magic arrow");
+        SpellFactory factory = new SpellFactory();
+        Spell spell = factory.createSpell(spellInfo);
+
+        spell.cast(creature);
+        assertEquals(0, creature.getCurrentHp());
+        //DMG przechodzący na pozostałe jednostki w przypadku stackowania ?
+    }
 }
