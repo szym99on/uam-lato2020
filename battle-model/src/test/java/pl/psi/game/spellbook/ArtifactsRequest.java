14bff3b903106c24686af34e686e5851e4133a54
SzmMar <marcelszmeterowicz@gmail.com>
2020-05-19 03:06:41 +0200

    fixed ArtifactRequest getDuration for Spellbook

--- battle-model/src/test/java/pl/psi/game/spellbook/ArtifactsRequest.java
+++ battle-model/src/test/java/pl/psi/game/spellbook/ArtifactsRequest.java
@@ -1,22 +1,17 @@
-//package pl.psi.game.spellbook;
-//
-//import org.junit.Test;
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//public class ArtifactsRequest {
-//
-//    @Test
-//    void shouldReturnSpellDuration(){
-//
-//        // mam artefakt, który wydłuża czas trwania spella o jedną turę
-//        // wiem, że muszę tego spella udekorować, czyli musze wymagac od grupy od czarow zeby
-//        // mieli metodę "dealBuffDuration" i potem w dekoratorze zwiększyć ją o jeden?
-//        // czy udekorować obiekt Spell i mu zwiekszyć długosc dzialania analogicznie do przykładu z zajęć dotyczącego
-//        // kupowania kawy i zwiększania jej ceny poprzez mleko itp
-//
-//        SpellInfo spell = SpellBookInfoFactory.getSpell(SpellBookInfoFactory.AIR_SHIELD);
-//        assertEquals("Air shield", spell.getName());
-//        assertEquals(1, spell.getDuration());
-//    }
-//
-//}
+package pl.psi.game.spellbook;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ArtifactsRequest {
+
+    @Disabled
+    @Test
+    void shouldReturnSpellDuration(){
+        SpellInfo spell = SpellBookInfoFactory.getSpell("AIR_SHIELD");
+
+        assertEquals(1, spell.getDuration());
+    }
+
+}
