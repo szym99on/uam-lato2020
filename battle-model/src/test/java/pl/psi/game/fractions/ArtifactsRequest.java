e548d9cb92f88425266e02c213e28f96edaefb96
SzmMar <marcelszmeterowicz@gmail.com>
2020-05-20 14:38:03 +0200

    created ArtifactsRequest for Fractions

---
+++ battle-model/src/test/java/pl/psi/game/fractions/ArtifactsRequest.java
@@ -0,0 +1,8 @@
+package pl.psi.game.fractions;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+public class ArtifactsRequest {
+}
