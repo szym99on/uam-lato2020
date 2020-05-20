53a62d9f31527e59184f21c9a6a45ad43f939556
Monika <monikakuczkowska@gmail.com>
2020-05-20 19:10:43 +0200

    necropolis creatures statistics changed to upgrated

--- battle-model/src/main/java/pl/psi/game/fractions/NecropolisCreatureFactory.java
+++ battle-model/src/main/java/pl/psi/game/fractions/NecropolisCreatureFactory.java
@@ -7,19 +7,19 @@ public class NecropolisCreatureFactory {
     public Creature createCreature(int aTier) {
         switch (aTier) {
             case 1:
-                return Creature.builder().aMaxHp(6).aAttack(Range.closed(1, 3)).aName("Skeleton").aArmor(4).aMoveRange(4).build();
+                return Creature.builder().aMaxHp(6).aAttack(Range.closed(1, 3)).aName("Skeleton Warrior").aArmor(6).aMoveRange(5).build();
             case 2:
-                return Creature.builder().aMaxHp(15).aAttack(Range.closed(2, 3)).aName("Walking Dead").aArmor(5).aMoveRange(3).build();
+                return Creature.builder().aMaxHp(20).aAttack(Range.closed(2, 3)).aName("Zombie").aArmor(5).aMoveRange(4).build();
             case 3:
-                return Creature.builder().aMaxHp(18).aAttack(Range.closed(3, 5)).aName("Wight").aArmor(7).aMoveRange(5).build();
+                return Creature.builder().aMaxHp(18).aAttack(Range.closed(3, 5)).aName("Wraith").aArmor(7).aMoveRange(7).build();
             case 4:
-                return Creature.builder().aMaxHp(30).aAttack(Range.closed(5, 8)).aName("Vampire").aArmor(9).aMoveRange(6).build();
+                return Creature.builder().aMaxHp(40).aAttack(Range.closed(5, 8)).aName("Vampire Lord").aArmor(10).aMoveRange(9).build();
             case 5:
-                return Creature.builder().aMaxHp(30).aAttack(Range.closed(11, 13)).aName("Lich").aArmor(10).aMoveRange(6).build();
+                return Creature.builder().aMaxHp(40).aAttack(Range.closed(11, 15)).aName("Power Lich").aArmor(10).aMoveRange(7).build();
             case 6:
-                return Creature.builder().aMaxHp(120).aAttack(Range.closed(15, 30)).aName("Black Knight").aArmor(16).aMoveRange(7).build();
+                return Creature.builder().aMaxHp(120).aAttack(Range.closed(15, 30)).aName("Dread Knight").aArmor(18).aMoveRange(9).build();
             case 7:
-                return Creature.builder().aMaxHp(150).aAttack(Range.closed(25, 50)).aName("Bone Dragon").aArmor(15).aMoveRange(9).build();
+                return Creature.builder().aMaxHp(200).aAttack(Range.closed(25, 50)).aName("Ghost Dragon").aArmor(17).aMoveRange(14).build();
             default:
                 throw new IllegalArgumentException("You have to pass tier 1-7");
         }
