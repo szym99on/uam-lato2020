f9c29a1c362ca49ad817a715f9c2a59834903f67
SzmMar <marcelszmeterowicz@gmail.com>
2020-05-20 14:39:32 +0200

    added returnCreatureStats ArtifactsRequest for Fractions

--- battle-model/src/test/java/pl/psi/game/fractions/ArtifactsRequest.java
+++ battle-model/src/test/java/pl/psi/game/fractions/ArtifactsRequest.java
@@ -5,4 +5,17 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 public class ArtifactsRequest {
+
+    @Disabled
+    @Test
+    void shouldReturnCreatureStats(){
+//        Creature creature = Creature.builder().aAttack(Range.closed(2, 2)).aMaxHp(10).aArmor(5).aMoveRange(2).build();
+
+//        assertEquals(Range.closed(2, 2), creature.getAttack());
+//        assertEquals(10, creature.getMaxHP());
+//        assertEquals(10, creature.getCurrentHP());
+//        assertEquals(5, creature.getArmor());
+//        assertEquals(2, creature.getMoveRange());
+    }
+
 }
