34f247e007d5a9ffbf395dd76524c513fcf03dc7
strus100 <danek130@gmail.com>
2020-05-22 19:11:53 +0200

    MoveInfo added

--- economy-model/src/main/java/pl/psi/game/move/MoveInfo.java
+++ economy-model/src/main/java/pl/psi/game/move/MoveInfo.java
@@ -1,4 +1,24 @@
 package pl.psi.game.move;
 
 public class MoveInfo {
+
+    private final Pathfinding airMagic;
+    private final AirMagic pathFinding;
+
+    public enum AirMagic {
+        BASIC,
+        ADVANCE,
+        EXPERT
+    }
+
+    public enum Pathfinding {
+        BASIC,
+        ADVANCE,
+        EXPERT
+    }
+
+    MoveInfo(Pathfinding aAirMagic,AirMagic aPathFinding){
+        airMagic = aAirMagic;
+        pathFinding = aPathFinding;
+    }
 }
