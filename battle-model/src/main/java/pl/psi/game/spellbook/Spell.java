80da50f04c95645c3489b6f56711f5e186f334ff
Mateusz Wawrzonek <matwaw4@st.amu.edu.pl>
2020-05-22 10:28:03 +0200

    Add cast BuffDebuff example

--- battle-model/src/main/java/pl/psi/game/spellbook/Spell.java
+++ battle-model/src/main/java/pl/psi/game/spellbook/Spell.java
@@ -4,13 +4,31 @@ import lombok.Builder;
 import pl.psi.game.fractions.Creature;
 
 public class Spell {
+
+    protected final String name;
+    private final String description;
+    private final int tier;
+    private String duration;
+    private int cost;
+    private int manaCost;
+    private SpellInfo.Type type;
+
     @Builder
-    public Spell (String aName) {
+    public Spell (String aName, String aDescription, int aCost, int aTier, int aManaCost, SpellInfo.Type aType, String aDuration) {
+        name = aName;
+        description = aDescription;
+        tier = aTier;
+        duration = aDuration;
+        cost = aCost;
+        manaCost = aManaCost;
+        type = aType;
+    }
+
+    void cast (int x, int y){
 
     }
 
-    //TODO: cast(Point) zaklęcia będą rzucane na pole.
-    void cast(Creature creature){
+    void cast (Creature creature){
         //creature.acceptSpell();
     }
 }
