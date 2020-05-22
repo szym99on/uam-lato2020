26f6456ff7633dd60d3285ba7732b44f74c09333
strus100 <danek130@gmail.com>
2020-05-22 18:07:42 +0200

    Update Move function by walking creature

--- battle-model/src/main/java/pl/psi/game/move/MoveStrategyWalk.java
+++ battle-model/src/main/java/pl/psi/game/move/MoveStrategyWalk.java
@@ -23,9 +23,34 @@ public class MoveStrategyWalk implements MoveStrategyIf {
     @Override
     public void move(int x, int y) {
         Point oldPosition = activeCreature.getKey();
-        board.move(x,y,activeCreature.getValue());
+
+        double xDistance = Math.abs(x - oldPosition.getX());
+        double yDistance = Math.abs(y - oldPosition.getY());
+
+        for (int i = 0; i < Math.max(xDistance,yDistance) ; i++) {
+            if(i < xDistance ) {
+                board.move(oldPosition.x + goDirection(x, oldPosition.x), oldPosition.y, activeCreature.getValue());
+            }
+            if(i < yDistance ) {
+                board.move(oldPosition.x, oldPosition.y + goDirection(y, oldPosition.y), activeCreature.getValue());
+            }
+
+
+        }
+        //TODO Can I should move this code to for -> if ?
         activeCreature = new AbstractMap.SimpleEntry<>(new Point(x,y), activeCreature.getValue());
         propertyChangeSupport.firePropertyChange(GameEngine.CREATURE_MOVED, oldPosition, activeCreature.getKey());
+    }
 
+    int goDirection(int x, int oldX){
+        if(x > oldX) {
+            return 1;
+        }
+
+        if(x < oldX) {
+            return -1;
+        }
+
+        return 0;
     }
 }
