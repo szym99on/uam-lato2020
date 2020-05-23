d86a0664a824eaa3047f4dddac8ff552ed7606b5
software-wizard <przemek@software-wizard.pl>
2020-05-23 12:33:55 +0200

    artifacts comments

--- battle-model/src/main/java/pl/psi/game/artifacts/Artifact.java
+++ battle-model/src/main/java/pl/psi/game/artifacts/Artifact.java
@@ -3,26 +3,24 @@ package pl.psi.game.artifacts;
 import com.google.common.collect.Range;
 import lombok.Builder;
 import lombok.Getter;
-import pl.psi.game.hero.EconomyHero;
 import pl.psi.game.hero.artifacts.ArtifactInfo;
+import pl.psi.game.hero.converter.Hero;
 import pl.psi.game.move.GuiTileIf;
 
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 
 @Getter
-public class Artifact{
+public abstract class Artifact{
 
     private final String name;
     private final ArtifactInfo.Location location;
 
-    @Builder
     public Artifact(String aName, ArtifactInfo.Location aLocation) {
         name = aName;
         location = aLocation;
     }
 
 
-    public void apply(EconomyHero hero) {
-    }
+    public abstract void apply(Hero aHero);
 }
--- battle-model/src/main/java/pl/psi/game/artifacts/ArtifactFactory.java
+++ battle-model/src/main/java/pl/psi/game/artifacts/ArtifactFactory.java
@@ -3,7 +3,16 @@ package pl.psi.game.artifacts;
 import pl.psi.game.hero.artifacts.ArtifactInfo;
 
 public class ArtifactFactory {
-    public Artifact createArtifact(ArtifactInfo artifactInfo){
-        return Artifact.builder().aName(artifactInfo.getName()).aLocation(artifactInfo.getLocation()).build();
+    public static Artifact createFromInfo(ArtifactInfo aArtifactInfo) {
+
+        return null;
+    }
+
+    //PW you should return concrete class not Artifact. For example:
+    // SKULL_HELMET should return Artifact for heroes (more specific HeroAttributesArtifact)
+    // BOOTS_OF_POLARITY should return Artifact for creature ( more specific MagicResistanceForCreatureArtifact)
+    public static Artifact createArtifact(ArtifactInfo artifactInfo){
+//        return Artifact.builder().aName(artifactInfo.getName()).aLocation(artifactInfo.getLocation()).build();
+        return null;
     }
 }
