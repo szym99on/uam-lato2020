763b7a5e76040ad3520122d083cfd199c731ccdb
software-wizard <przemek@software-wizard.pl>
2020-05-23 12:45:10 +0200

    small fraction comment

--- battle-model/src/main/java/pl/psi/game/fractions/CreatureAbstractFactory.java
+++ battle-model/src/main/java/pl/psi/game/fractions/CreatureAbstractFactory.java
@@ -30,6 +30,12 @@ public class CreatureAbstractFactory {
 				break;
 			case "Grand Elf":
 			case "Wolf Raider":
+				//PW It's not working like that. You not multiply damage. You have to really attack 2 times.
+				// UseCase:
+				// creature with 10 amount in stack attacks defender
+				// deal 10*(attack-defence) damage
+				// defender counterattack and kill 3 unit in stack
+				// attacker attack second time but with 7 in stack so deal 7*(attack-defence) damage
 				creature.setDealDamageCountStrategy(new DealDamageCounterDoubleStrategy());
 				break;
 			case "Ancient Behemoth":
