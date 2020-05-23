e9e67f48823e49ccd2d812a43008ae99289a5b52
Mikolaj Pokrywka <mp7961@gmail.com>
2020-05-24 00:00:53 +0200

    added 4 test GetElementalAndPutHin

--- economy-model/src/main/java/pl/psi/game/spellbook/SpellBookInfoFactory.java
+++ economy-model/src/main/java/pl/psi/game/spellbook/SpellBookInfoFactory.java
@@ -21,10 +21,10 @@ public class SpellBookInfoFactory {
     private final static String WEAKNESS = "Weakness";
     private final static String PRAYER = "Prayer";
 
-    private final static String AIR_ELEMENTAL = "Summon air elemental";
-    private final static String FIRE_ELEMENTAL = "Summon fire elemental";
-    private final static String EARTH_ELEMENTAL = "Summon earth elemental";
-    private final static String WATER_ELEMENTAL = "Summon water elemental";
+    private final static String STORM_ELEMENTAL = "Summon storm elemental";
+    private final static String ENERGY_ELEMENTAL = "Summon energy elemental";
+    private final static String MAGMA_ELEMENTAL = "Summon magma elemental";
+    private final static String ICE_ELEMENTAL = "Summon ice elemental";
 
     private final static List<SpellInfo> spellList = new ArrayList<>();
     {
@@ -51,10 +51,10 @@ public class SpellBookInfoFactory {
         spellList.add(SpellInfo.builder().aCost(1).aDescription("Reduces the selected enemy unit's attack strength.").aDuration("1/magic power").aManaCost(8).aName(WEAKNESS).aLevel(2).aType(SpellInfo.Type.WATER).build());
         spellList.add(SpellInfo.builder().aCost(1).aDescription("Bestows a bonus to the attack strength, defense strength and speed of the selected unit.").aDuration("1/magic power").aManaCost(16).aName(PRAYER).aLevel(4).aType(SpellInfo.Type.WATER).build());
 
-        spellList.add(SpellInfo.builder().aCost(1).aDescription("Allows you to summon elementals. Once cast, no other elemental types may be summoned.").aDuration("Combat").aManaCost(25).aName(AIR_ELEMENTAL).aLevel(5).aType(SpellInfo.Type.AIR).build());
-        spellList.add(SpellInfo.builder().aCost(1).aDescription("Allows you to summon elementals. Once cast, no other elemental types may be summoned.").aDuration("Combat").aManaCost(25).aName(FIRE_ELEMENTAL).aLevel(5).aType(SpellInfo.Type.FIRE).build());
-        spellList.add(SpellInfo.builder().aCost(1).aDescription("Allows you to summon elementals. Once cast, no other elemental types may be summoned.").aDuration("Combat").aManaCost(25).aName(EARTH_ELEMENTAL).aLevel(5).aType(SpellInfo.Type.EARTH).build());
-        spellList.add(SpellInfo.builder().aCost(1).aDescription("Allows you to summon elementals. Once cast, no other elemental types may be summoned.").aDuration("Combat").aManaCost(25).aName(WATER_ELEMENTAL).aLevel(5).aType(SpellInfo.Type.WATER).build());
+        spellList.add(SpellInfo.builder().aCost(1).aDescription("Allows you to summon elementals. Once cast, no other elemental types may be summoned.").aDuration("Combat").aManaCost(25).aName(STORM_ELEMENTAL).aLevel(5).aType(SpellInfo.Type.AIR).build());
+        spellList.add(SpellInfo.builder().aCost(1).aDescription("Allows you to summon elementals. Once cast, no other elemental types may be summoned.").aDuration("Combat").aManaCost(25).aName(ENERGY_ELEMENTAL).aLevel(5).aType(SpellInfo.Type.FIRE).build());
+        spellList.add(SpellInfo.builder().aCost(1).aDescription("Allows you to summon elementals. Once cast, no other elemental types may be summoned.").aDuration("Combat").aManaCost(25).aName(MAGMA_ELEMENTAL).aLevel(5).aType(SpellInfo.Type.EARTH).build());
+        spellList.add(SpellInfo.builder().aCost(1).aDescription("Allows you to summon elementals. Once cast, no other elemental types may be summoned.").aDuration("Combat").aManaCost(25).aName(ICE_ELEMENTAL).aLevel(5).aType(SpellInfo.Type.WATER).build());
     }
 
     public static SpellInfo getSpell(String aName){
