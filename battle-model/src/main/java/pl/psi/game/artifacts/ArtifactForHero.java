1aa92e26108a260bec47de2ef56cb4ab2ed2c100
SzmMar <marcelszmeterowicz@gmail.com>
2020-05-24 18:51:03 +0200

    added basic implementation in ArtifactForHero

--- battle-model/src/main/java/pl/psi/game/artifacts/ArtifactForHero.java
+++ battle-model/src/main/java/pl/psi/game/artifacts/ArtifactForHero.java
@@ -1,4 +1,13 @@
 package pl.psi.game.artifacts;
 
-public class ArtifactForHero {
+import pl.psi.game.hero.artifacts.ArtifactInfo;
+import pl.psi.game.hero.converter.Hero;
+
+public class ArtifactForHero extends Artifact{
+    public ArtifactForHero(String aName, ArtifactInfo.Location aLocation) { super(aName, aLocation); }
+
+    @Override
+    public void apply(Hero aHero) {
+
+    }
 }
