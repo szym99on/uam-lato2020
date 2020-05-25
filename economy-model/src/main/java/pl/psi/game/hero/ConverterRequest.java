5b776a5e78c887bf3c6ca767d8dc24125e6040e8
thebuntaro <thebuntaro@wp.pl>
2020-05-25 17:47:46 +0200

    create Request for EconomyHero

---
+++ economy-model/src/main/java/pl/psi/game/hero/ConverterRequest.java
@@ -0,0 +1,22 @@
+package pl.psi.game.hero;
+
+import org.junit.Test;
+
+public class ConverterRequest {
+    /*@Test
+    void EconomyHeroBuildFromHeroInfo()
+    {
+        HeroInfo heroInfo = HeroInfoFactory.getHeroInfoByName(HeroInfoFactory.EDRIC);
+        EconomyHero economyHero = EconomyHero.builder().aHeroInfo(heroInfo).build();
+    }*/
+    /*@Test
+    void EconomyHeroGetArtifactsShouldBePublic()
+    {
+
+    }*/
+    /*@Test
+    void EconomyHeroGetSpellsShouldBePublic()
+    {
+
+    }*/
+}
