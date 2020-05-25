3ebd8b7cc0172533641722345b80026176dd6c60
Jakub Przybyla <kubaprzybya@gmail.com>
2020-05-25 13:56:13 +0200

    some little changes

--- battle-model/src/main/java/pl/psi/game/artifacts/ArtifactFactory.java
+++ battle-model/src/main/java/pl/psi/game/artifacts/ArtifactFactory.java
@@ -17,138 +17,124 @@ public class ArtifactFactory {
         if (artifactInfo.equals(HELM_OF_THE_ALABASTER_UNICORN)){
             return new HeroStatisticBuffArtifact(0,0,0,1);
         }
-        if (artifactInfo.equals(SKULL_HELMET)){
+        else if (artifactInfo.equals(SKULL_HELMET)){
             return new HeroStatisticBuffArtifact(0,0,0,2);
         }
-        if (artifactInfo.equals(HELM_OF_CHAOS)){
+        else if (artifactInfo.equals(HELM_OF_CHAOS)){
             return new HeroStatisticBuffArtifact(0,0,0,3);
         }
-        if (artifactInfo.equals(CROWN_OF_THE_SUPREME_MAGI)){
+        else if (artifactInfo.equals(CROWN_OF_THE_SUPREME_MAGI)){
             return new HeroStatisticBuffArtifact(0,0,0,4);
         }
-        if (artifactInfo.equals(HELLSTORM_HELMET)){
+        else if (artifactInfo.equals(HELLSTORM_HELMET)){
             return new HeroStatisticBuffArtifact(0,0,0,5);
         }
-        if (artifactInfo.equals(NECKLACE_OF_DRAGONTEETH)){
+        else if (artifactInfo.equals(NECKLACE_OF_DRAGONTEETH)){
             return new HeroStatisticBuffArtifact(0,0,3,3);
         }
-        if (artifactInfo.equals(CELESTIAL_NECKLACE_OF_BLISS)){
+        else if (artifactInfo.equals(CELESTIAL_NECKLACE_OF_BLISS)){
             return new HeroStatisticBuffArtifact(3,3,3,3);
         }
-        if (artifactInfo.equals(COLLAR_OF_CONJURING)){
+        else if (artifactInfo.equals(COLLAR_OF_CONJURING)){
             return new SpellBuffArtifact(1);
         }
-//        if (artifactInfo.equals(PENDANT_OF_DISPASSION)){
+//        else if (artifactInfo.equals(PENDANT_OF_DISPASSION)){
 //            return new CreatureBuffArtifact(0, 0, 0);
 //        }
-//        if (artifactInfo.equals(PENDANT_OF_LIFE)){
+//        else if (artifactInfo.equals(PENDANT_OF_LIFE)){
 //            return new CreatureBuffArtifact(0, 0, 0);
 //        }
-        if (artifactInfo.equals(BREASTPLATE_OF_PETRIFIED_WOOD)){
+        else if (artifactInfo.equals(BREASTPLATE_OF_PETRIFIED_WOOD)){
             return new HeroStatisticBuffArtifact(0,0,1,0);
         }
-        if (artifactInfo.equals(RIB_CAGE)){
+        else if (artifactInfo.equals(RIB_CAGE)){
             return new HeroStatisticBuffArtifact(0,0,2,0);
         }
-        if (artifactInfo.equals(DRAGON_SCALE_ARMOR)){
+        else if (artifactInfo.equals(DRAGON_SCALE_ARMOR)){
             return new HeroStatisticBuffArtifact(4,4,0,0);
         }
-        if (artifactInfo.equals(ARMOR_OF_WONDER)){
+        else if (artifactInfo.equals(ARMOR_OF_WONDER)){
             return new HeroStatisticBuffArtifact(1,1,1,1);
         }
-        if (artifactInfo.equals(TITANS_CUIRAS)){
+        else if (artifactInfo.equals(TITANS_CUIRAS)){
             return new HeroStatisticBuffArtifact(0,0,10,-2);
         }
-        if (artifactInfo.equals(CAPE_OF_CONJURING)){
+        else if (artifactInfo.equals(CAPE_OF_CONJURING)){
             return new SpellBuffArtifact(3);
         }
-        if (artifactInfo.equals(DRAGON_WING_TABARD)){
+        else if (artifactInfo.equals(DRAGON_WING_TABARD)){
             return new HeroStatisticBuffArtifact(0,0,2,2);
         }
-        if (artifactInfo.equals(SURCOAT_OF_COUNTERPOISE)){
+        else if (artifactInfo.equals(SURCOAT_OF_COUNTERPOISE)){
             return new CreatureBuffArtifact(0,10,0);
         }
-//        if (artifactInfo.equals(RECANTERS_CLOAK)){
+//        else if (artifactInfo.equals(RECANTERS_CLOAK)){
 //            return new SpellBuffArtifact(0);
 //        }
-        if (artifactInfo.equals(CAPE_OF_VELOCITY)){
+        else if (artifactInfo.equals(CAPE_OF_VELOCITY)){
             return new CreatureBuffArtifact(2,0,0);
         }
-        if (artifactInfo.equals(CENTAURS_AX)){
+        else if (artifactInfo.equals(CENTAURS_AX)){
             return new HeroStatisticBuffArtifact(2,0,0,0);
         }
-        if (artifactInfo.equals(BLACKSHARD_OF_THE_DEAD_KNIGHT)){
+        else if (artifactInfo.equals(BLACKSHARD_OF_THE_DEAD_KNIGHT)){
             return new HeroStatisticBuffArtifact(3,0,0,0);
         }
-        if (artifactInfo.equals(GREATER_GNOLLS_FLAIL)){
+        else if (artifactInfo.equals(GREATER_GNOLLS_FLAIL)){
             return new HeroStatisticBuffArtifact(4,0,0,0);
         }
-        if (artifactInfo.equals(OGRES_CLUB_OF_HAVOC)){
+        else if (artifactInfo.equals(OGRES_CLUB_OF_HAVOC)){
             return new HeroStatisticBuffArtifact(5,0,0,0);
         }
-        if (artifactInfo.equals(TITANS_GLADIUS)){
+        else if (artifactInfo.equals(TITANS_GLADIUS)){
             return new HeroStatisticBuffArtifact(12,-3,0,0);
         }
-        if (artifactInfo.equals(SHIELD_OF_THE_DWARVEN_LORDS)){
+        else if (artifactInfo.equals(SHIELD_OF_THE_DWARVEN_LORDS)){
             return new HeroStatisticBuffArtifact(0,2,0,0);
         }
-        if (artifactInfo.equals(SHIELD_OF_THE_YAWNING_DEAD)){
+        else if (artifactInfo.equals(SHIELD_OF_THE_YAWNING_DEAD)){
             return new HeroStatisticBuffArtifact(0,3,0,0);
         }
-        if (artifactInfo.equals(BUCKLER_OF_THE_GNOLL_KING)){
+        else if (artifactInfo.equals(BUCKLER_OF_THE_GNOLL_KING)){
             return new HeroStatisticBuffArtifact(0,4,0,0);
         }
-        if (artifactInfo.equals(TARG_OF_THE_RAMPAGING_OGRE)){
+        else if (artifactInfo.equals(TARG_OF_THE_RAMPAGING_OGRE)){
             return new HeroStatisticBuffArtifact(0,5,0,0);
         }
-        if (artifactInfo.equals(SENTINELS_SHIELD)){
+        else if (artifactInfo.equals(SENTINELS_SHIELD)){
             return new HeroStatisticBuffArtifact(-3,12,0,0);
         }
-        if (artifactInfo.equals(RING_OF_VITALITY)){
+        else if (artifactInfo.equals(RING_OF_VITALITY)){
             return new CreatureBuffArtifact(0,0,1);
         }
-        if (artifactInfo.equals(RING_OF_LIFE)){
+        else if (artifactInfo.equals(RING_OF_LIFE)){
             return new CreatureBuffArtifact(0,0,2);
         }
-        if (artifactInfo.equals(RING_OF_CONJURING)){
+        else if (artifactInfo.equals(RING_OF_CONJURING)){
             return new SpellBuffArtifact(2);
         }
-        if (artifactInfo.equals(RING_OF_THE_WAYFARER)){
+        else if (artifactInfo.equals(RING_OF_THE_WAYFARER)){
             return new CreatureBuffArtifact(1,0,0);
         }
-        if (artifactInfo.equals(QUIET_EYE_OF_THE_DRAGON)){
+        else if (artifactInfo.equals(QUIET_EYE_OF_THE_DRAGON)){
             return new HeroStatisticBuffArtifact(1,1,0,0);
         }
-        if (artifactInfo.equals(SANDALS_OF_THE_SAINT)){
+        else if (artifactInfo.equals(SANDALS_OF_THE_SAINT)){
             return new HeroStatisticBuffArtifact(2,2,2,2);
         }
-        if (artifactInfo.equals(DRAGONBONE_GREAVES)){
+        else if (artifactInfo.equals(DRAGONBONE_GREAVES)){
             return new HeroStatisticBuffArtifact(0,0,1,1);
         }
-        if (artifactInfo.equals(BOOTS_OF_POLARITY)){
+        else if (artifactInfo.equals(BOOTS_OF_POLARITY)){
             return new CreatureBuffArtifact(0,15,0);
         }
-        if (artifactInfo.equals(GUARDIANS_BOOTS)){
+        else if (artifactInfo.equals(GUARDIANS_BOOTS)){
             return new HeroStatisticBuffArtifact(0,3,0,0);
         }
-        if (artifactInfo.equals(MAGISTERS_SANDALS)){
+        else if (artifactInfo.equals(MAGISTERS_SANDALS)){
             return new HeroStatisticBuffArtifact(0,0,2,0);
         }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        return null;
+        else 
+            return null;
     }
 }
