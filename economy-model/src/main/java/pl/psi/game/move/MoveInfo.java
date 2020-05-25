0632a29a9c65194ef0b4645dbc20b0fdb8723d41
strus100 <danek130@gmail.com>
2020-05-25 17:56:03 +0200

    MoveInfo Created

--- economy-model/src/main/java/pl/psi/game/move/MoveInfo.java
+++ economy-model/src/main/java/pl/psi/game/move/MoveInfo.java
@@ -1,4 +1,25 @@
 package pl.psi.game.move;
 
+import lombok.Builder;
+
 public class MoveInfo {
+
+    private final boolean canFly;
+
+    public enum AirMagic{
+        BASIC,
+        ADVANCE,
+        EXPERT
+    }
+
+    public enum Pathfinding{
+        BASIC,
+        ADVANCE,
+        EXPERT
+    }
+
+    @Builder
+    public MoveInfo(boolean aCanFly){
+        canFly = aCanFly;
+    }
 }
