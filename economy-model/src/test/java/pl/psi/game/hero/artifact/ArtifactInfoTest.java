cdff8b279d2e67d26288d7d87f1eb6804f2838d7
Jakub Przybyla <kubaprzybya@gmail.com>
2020-05-25 20:39:08 +0200

    extend first test artifact info

--- economy-model/src/test/java/pl/psi/game/hero/artifact/ArtifactInfoTest.java
+++ economy-model/src/test/java/pl/psi/game/hero/artifact/ArtifactInfoTest.java
@@ -5,8 +5,8 @@ import org.junit.jupiter.api.Test;
 import pl.psi.game.hero.artifacts.ArtifactsInfoFactory;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static pl.psi.game.hero.artifacts.ArtifactsInfoFactory.HELM_OF_THE_ALABASTER_UNICORN;
-import static pl.psi.game.hero.artifacts.ArtifactsInfoFactory.getArtifact;
+import static pl.psi.game.hero.artifacts.ArtifactInfo.Location.*;
+import static pl.psi.game.hero.artifacts.ArtifactsInfoFactory.*;
 
 public class ArtifactInfoTest {
     @BeforeAll
@@ -18,6 +18,22 @@ public class ArtifactInfoTest {
     void shouldFindAppropriateArtifactByName(){
         assertEquals(HELM_OF_THE_ALABASTER_UNICORN,getArtifact(HELM_OF_THE_ALABASTER_UNICORN).getName());
         assertEquals(1000,getArtifact(HELM_OF_THE_ALABASTER_UNICORN).getCost());
+        assertEquals(HEAD, getArtifact(HELM_OF_THE_ALABASTER_UNICORN).getLocation());
 
+        assertEquals(SKULL_HELMET,getArtifact(SKULL_HELMET).getName());
+        assertEquals(3000,getArtifact(SKULL_HELMET).getCost());
+        assertEquals(HEAD, getArtifact(SKULL_HELMET).getLocation());
+
+        assertEquals(BLACKSHARD_OF_THE_DEAD_KNIGHT,getArtifact(BLACKSHARD_OF_THE_DEAD_KNIGHT).getName());
+        assertEquals(3000,getArtifact(BLACKSHARD_OF_THE_DEAD_KNIGHT).getCost());
+        assertEquals(RIGHT_HAND, getArtifact(BLACKSHARD_OF_THE_DEAD_KNIGHT).getLocation());
+
+        assertEquals(RING_OF_VITALITY,getArtifact(RING_OF_VITALITY).getName());
+        assertEquals(5000,getArtifact(RING_OF_VITALITY).getCost());
+        assertEquals(FINGERS, getArtifact(RING_OF_VITALITY).getLocation());
+
+        assertEquals(GUARDIANS_BOOTS,getArtifact(GUARDIANS_BOOTS).getName());
+        assertEquals(3000,getArtifact(GUARDIANS_BOOTS).getCost());
+        assertEquals(FEET, getArtifact(GUARDIANS_BOOTS).getLocation());
     }
 }
