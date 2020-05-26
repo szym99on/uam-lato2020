cd43b0b88241c51d64973cdc00f807e5c15b90fe
strus100 <danek130@gmail.com>
2020-05-26 15:28:22 +0200

    moveInfo refactor

--- economy-model/src/main/java/pl/psi/game/move/MoveInfo.java
+++ economy-model/src/main/java/pl/psi/game/move/MoveInfo.java
@@ -4,25 +4,14 @@ import lombok.Builder;
 
 public class MoveInfo {
 
+    
+    private final String groundType;
+    private final boolean diagonalMove;
 
-    private final boolean onlyFly;
-    private final String type;
-
-    public enum AirMagic{
-        BASIC,
-        ADVANCE,
-        EXPERT
-    }
-
-    public enum Pathfinding{
-        BASIC,
-        ADVANCE,
-        EXPERT
-    }
 
     @Builder
-    public MoveInfo(boolean aOnlyFly,String aType){
-        onlyFly = aOnlyFly;
-        type = aType;
+    public MoveInfo(boolean aDiagonalMove,String aGroundType){
+        diagonalMove = aDiagonalMove;
+        groundType = aGroundType;
     }
 }
--- economy-model/src/main/java/pl/psi/game/move/MoveInfoFactory.java
+++ economy-model/src/main/java/pl/psi/game/move/MoveInfoFactory.java
@@ -2,5 +2,17 @@ package pl.psi.game.move;
 
 public class MoveInfoFactory {
 
+    public enum AirMagic{
+        BASIC,
+        ADVANCE,
+        EXPERT
+    }
+
+    public enum Pathfinding{
+        BASIC,
+        ADVANCE,
+        EXPERT
+    }
+
 
 }
