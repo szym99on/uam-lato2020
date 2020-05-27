d961a981be1f08978537f760830250240642256a
strus100 <danek130@gmail.com>
2020-05-27 07:52:37 +0200

    ObstacleInfo & InfoFactory created

--- economy-model/src/main/java/pl/psi/game/move/ObstacleInfo.java
+++ economy-model/src/main/java/pl/psi/game/move/ObstacleInfo.java
@@ -2,4 +2,9 @@ package pl.psi.game.move;
 
 public class ObstacleInfo {
 
+    private String name;
+    private final boolean isObstacle = true;
+    ObstacleInfo(String aName){
+        name = aName;
+    }
 }
--- economy-model/src/main/java/pl/psi/game/move/ObstacleInfoFactory.java
+++ economy-model/src/main/java/pl/psi/game/move/ObstacleInfoFactory.java
@@ -1,4 +1,12 @@
 package pl.psi.game.move;
 
+import java.util.ArrayList;
+
 public class ObstacleInfoFactory {
+
+    private final ArrayList<ObstacleInfo> obstacle = new ArrayList<>() {
+        {
+            add(new ObstacleInfo("impassable"));
+        }
+    };
 }
