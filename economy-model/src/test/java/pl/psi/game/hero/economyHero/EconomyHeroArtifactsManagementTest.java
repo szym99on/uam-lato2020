beba95f43538a68af3332f6cee63c77f85b93f7f
Kacper <50482739+Kacper777777@users.noreply.github.com>
2020-05-28 19:53:55 +0200

    Moving artifacts tests to appropriate file

--- economy-model/src/test/java/pl/psi/game/hero/economyHero/EconomyHeroArtifactsManagementTest.java
+++ economy-model/src/test/java/pl/psi/game/hero/economyHero/EconomyHeroArtifactsManagementTest.java
@@ -1,4 +1,47 @@
 package pl.psi.game.hero.economyHero;
 
+import org.junit.jupiter.api.Test;
+import pl.psi.game.hero.artifacts.ArtifactInfo;
+import pl.psi.game.hero.artifacts.ArtifactsInfoFactory;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class EconomyHeroArtifactsManagementTest {
+
+    @Test
+    void sellArtifactShouldReturn75PercentOfOriginalPriceAndRemoveArtifact() throws Exception {
+        ArtifactInfo artifact = ArtifactsInfoFactory.getArtifact(ArtifactsInfoFactory.HELM_OF_THE_ALABASTER_UNICORN);
+        EconomyHero hero = EconomyHero.builder().aGold(2000).build();
+        hero.addArtifact(artifact);
+
+        hero.sellArtifact(artifact);
+
+        assertEquals(hero.getGold(), 2750);
+        assertFalse(hero.getArtifacts().contains(artifact));
+        assertEquals(hero.getArtifacts().size(), 0);
+    }
+
+    @Test
+    void buyArtifactShouldNotAddArtifactIfSlotAlreadyTaken() throws Exception {
+
+        assertThrows(Exception.class, () -> {
+            ArtifactInfo artifact = ArtifactsInfoFactory.getArtifact(ArtifactsInfoFactory.CAPE_OF_CONJURING);
+            EconomyHero hero = EconomyHero.builder().aGold(2000).build();
+
+            hero.buyArtifact(artifact);
+            hero.buyArtifact(artifact);
+        });
+    }
+    @Test
+    void  buyArtifactShouldAddArtifactIfLocationEmpty() throws Exception {
+        ArtifactInfo artifact = ArtifactsInfoFactory.getArtifact(ArtifactsInfoFactory.COLLAR_OF_CONJURING);
+        EconomyHero hero = EconomyHero.builder().aGold(2000).build();
+
+        hero.buyArtifact(artifact);
+
+        assertEquals(hero.getArtifact(artifact.getName()),artifact);
+        assertEquals(hero.getArtifacts().size(),1);
+
+    }
+
 }
