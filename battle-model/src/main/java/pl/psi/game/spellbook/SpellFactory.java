47663643f52c959439d88dcf35308cd0f070a422
Mateusz Wawrzonek <matwaw4@st.amu.edu.pl>
2020-05-28 09:09:28 +0200

    SpellFactory corrections

--- battle-model/src/main/java/pl/psi/game/spellbook/SpellFactory.java
+++ battle-model/src/main/java/pl/psi/game/spellbook/SpellFactory.java
@@ -5,11 +5,11 @@ public class SpellFactory {
     public Spell createSpell(SpellInfo spellInfo){
         switch (spellInfo.getName()){
             case SpellBookInfoFactory.MAGIC_ARROW :
-            case SpellBookInfoFactory.MAGMA_ELEMENTAL :
-            case SpellBookInfoFactory.WEAKNESS :
             case SpellBookInfoFactory.LIGHTNING_BOLT :
             case SpellBookInfoFactory.DESTROY_UNDEAD :
             case SpellBookInfoFactory.FIRE_BALL :
+                return null; //Wait for damageSpell implementation
+            case SpellBookInfoFactory.WEAKNESS :
             case SpellBookInfoFactory.HASTE :
             case SpellBookInfoFactory.AIR_SHIELD :
             case SpellBookInfoFactory.BLOODLUST :
@@ -17,10 +17,12 @@ public class SpellFactory {
             case SpellBookInfoFactory.SLOW :
             case SpellBookInfoFactory.STONESKIN :
             case SpellBookInfoFactory.PRAYER :
+                return BuffDebufSpell.builder().aName(spellInfo.getName()).aCost(spellInfo.getCost()).aDescription(spellInfo.getDescription()).aDuration(spellInfo.getDuration()).aManaCost(spellInfo.getManaCost()).aLevel(spellInfo.getLevel()).aType(spellInfo.getType()).build();
             case SpellBookInfoFactory.STORM_ELEMENTAL :
             case SpellBookInfoFactory.ENERGY_ELEMENTAL :
             case SpellBookInfoFactory.ICE_ELEMENTAL :
-                return BuffDebufSpell.builder().aName(spellInfo.getName()).aCost(spellInfo.getCost()).aDescription(spellInfo.getDescription()).aDuration(spellInfo.getDuration()).aManaCost(spellInfo.getManaCost()).aLevel(spellInfo.getLevel()).aType(spellInfo.getType()).build();
+            case SpellBookInfoFactory.MAGMA_ELEMENTAL :
+                return null; //Wait for summonUnits implementation
             default:
                 throw new IllegalArgumentException("This spell doesn't exist");
         }
