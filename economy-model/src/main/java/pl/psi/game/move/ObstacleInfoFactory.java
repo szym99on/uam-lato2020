91ce5e87812b3eea847209763039628a2d32bcec
shaaqu <pawel.lukaszewicz99@gmail.com>
2020-05-29 14:22:37 +0200

    ObstacleInfoFactory.java updated

--- economy-model/src/main/java/pl/psi/game/move/ObstacleInfoFactory.java
+++ economy-model/src/main/java/pl/psi/game/move/ObstacleInfoFactory.java
@@ -6,7 +6,9 @@ public class ObstacleInfoFactory {
 
     private final ArrayList<ObstacleInfo> obstacle = new ArrayList<>() {
         {
-            add(new ObstacleInfo("impassable"));
+            add(ObstacleInfo.builder().aObstacleClass(ObstacleInfo.ObstacleClass.LAVA).build());
+            add(ObstacleInfo.builder().aObstacleClass(ObstacleInfo.ObstacleClass.MARSH).build());
+            add(ObstacleInfo.builder().aObstacleClass(ObstacleInfo.ObstacleClass.RIVER).build());
         }
     };
 }
