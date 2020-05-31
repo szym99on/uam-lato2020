4b024da88bb6c7db281972e8bcd98875785850ed
SzmMar <marcelszmeterowicz@gmail.com>
2020-05-31 02:44:43 +0200

    added explanatory comment in ArtifactInitializer after trying to work on it
    as of right now it's impossible to make progress

--- battle-gui/src/main/java/pl/psi/gui/ArtifactInitializer.java
+++ battle-gui/src/main/java/pl/psi/gui/ArtifactInitializer.java
@@ -10,7 +10,11 @@ public class ArtifactInitializer extends AbstractInitializer {
     EconomyHero getH1() {
         EconomyHero h1 = EconomyHero.builder().aGold(100000000).aHeroInfo(HeroInfoFactory.getHeroInfoByName(HeroInfoFactory.EDRIC)).build();
         try {
-            h1.buyCreature(FractionsInfoAbstractFactory.getFactory(FractionsInfoAbstractFactory.Fractions.NECROPOLIS).getCreatureByTier(1));
+            // gui is not refreshing, something is fucked up and after some rounds the whole thing looks like shit
+            // movement needs to fix their shit
+            // air units can move but stuff above makes it impossible to check anything
+            // welp, we have to wait ¯\_(ツ)_/¯
+            h1.buyCreature(FractionsInfoAbstractFactory.getFactory(FractionsInfoAbstractFactory.Fractions.NECROPOLIS).getCreatureByTier(7));
             h1.buyArtifact(ArtifactsInfoFactory.getArtifact(ArtifactsInfoFactory.BOOTS_OF_POLARITY));
         } catch (Exception aE) {
             aE.printStackTrace();
@@ -21,7 +25,7 @@ public class ArtifactInitializer extends AbstractInitializer {
     EconomyHero getH2() {
         EconomyHero h2 = EconomyHero.builder().aGold(100000000).aHeroInfo(HeroInfoFactory.getHeroInfoByName(HeroInfoFactory.ALAMAR)).build();
         try {
-            h2.buyCreature(FractionsInfoAbstractFactory.getFactory(FractionsInfoAbstractFactory.Fractions.RAMPART).getCreatureByTier(1));
+            h2.buyCreature(FractionsInfoAbstractFactory.getFactory(FractionsInfoAbstractFactory.Fractions.RAMPART).getCreatureByTier(7));
         } catch (Exception aE) {
         }
         return h2;
