3054b6b2eb8949157c18976dad6b006c068c49a4
thebuntaro <thebuntaro@wp.pl>
2020-05-31 17:48:32 +0200

    redone Request for spellbook

--- battle-model/src/test/java/pl/psi/game/spellbook/ConverterRequest.java
+++ battle-model/src/test/java/pl/psi/game/spellbook/ConverterRequest.java
@@ -1,16 +1,23 @@
 package pl.psi.game.spellbook;
 
+import org.junit.jupiter.api.Test;
+import pl.psi.game.hero.converter.Hero;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class ConverterRequest {
 
     /*@Test
-    void mapSpellInfoToSpell(){
-        SpellFactory.create(spellInfo);
-    }
-     */
-    /*@Test
-    void increasePowerOfSpell(){
-        spell.increasePower(aAmount);
-        //aAmount is hero.Power
-    }
-     */
+    void SpellBookContainSpells()
+    {
+        SpellFactory spellFactory = new SpellFactory();
+        List<Spell> spells = new ArrayList<Spell>();
+        spells.add(spellFactory.createSpell(SpellBookInfoFactory.getSpell(SpellBookInfoFactory.MAGIC_ARROW)));
+        Hero hero = Hero.builder().aSpells(spells).build();
+        SpellBook spellBook = new SpellBook(hero.getPower(),hero.getKnowledge()*10,spells);
+        assertEquals(spellBook.getSpells(),hero.getSpells());
+    }*/
 }
