c29b76fee0196cb73bf12fd531014a391af9032d
SzmMar <marcelszmeterowicz@gmail.com>
2020-06-05 16:51:23 +0200

    updated comments + added basic structure for ArtifactInitializer scenarios

--- battle-gui/src/main/java/pl/psi/gui/ArtifactInitializer.java
+++ battle-gui/src/main/java/pl/psi/gui/ArtifactInitializer.java
@@ -6,14 +6,19 @@ import pl.psi.game.hero.artifacts.ArtifactsInfoFactory;
 import pl.psi.game.hero.economyHero.EconomyHero;
 
 public class ArtifactInitializer extends AbstractInitializer {
+    // gui is not refreshing, something is wrong and after some rounds the whole thing looks like a mess
+    // movement needs to fix their stuff
+    // air units can move but stuff above makes it impossible to check anything
+    // units can't move directly to their right or left nor can they move directly above or underneath their position
+    // welp, we have to wait ¯\_(ツ)_/¯
+
 
     EconomyHero getH1() {
         EconomyHero h1 = EconomyHero.builder().aGold(100000000).aHeroInfo(HeroInfoFactory.getHeroInfoByName(HeroInfoFactory.EDRIC)).build();
         try {
-            // gui is not refreshing, something is wrong and after some rounds the whole thing looks like a mess
-            // movement needs to fix their stuff
-            // air units can move but stuff above makes it impossible to check anything
-            // welp, we have to wait ¯\_(ツ)_/¯
+//            ################################################################
+//            SCENARIO 1
+//            ################################################################
             h1.buyCreature(FractionsInfoAbstractFactory.getFactory(FractionsInfoAbstractFactory.Fractions.NECROPOLIS).getCreatureByTier(7));
             h1.buyArtifact(ArtifactsInfoFactory.getArtifact(ArtifactsInfoFactory.BOOTS_OF_POLARITY));
         } catch (Exception aE) {
@@ -25,6 +30,9 @@ public class ArtifactInitializer extends AbstractInitializer {
     EconomyHero getH2() {
         EconomyHero h2 = EconomyHero.builder().aGold(100000000).aHeroInfo(HeroInfoFactory.getHeroInfoByName(HeroInfoFactory.ALAMAR)).build();
         try {
+//            ################################################################
+//            SCENARIO 1
+//            ################################################################
             h2.buyCreature(FractionsInfoAbstractFactory.getFactory(FractionsInfoAbstractFactory.Fractions.RAMPART).getCreatureByTier(7));
         } catch (Exception aE) {
         }
