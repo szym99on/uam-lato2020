3a01e5e43c6d869651f02b7a8d35ee98fc643f79
shaaqu <pawel.lukaszewicz99@gmail.com>
2020-06-06 13:58:26 +0200

    DealDamageObstacleTest.java

---
+++ battle-model/src/test/java/pl/psi/game/move/DealDamageObstacleTest.java
@@ -0,0 +1,36 @@
+package pl.psi.game.move;
+
+import org.junit.Test;
+import pl.psi.game.fractions.Creature;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class DealDamageObstacleTest {
+
+    @Test
+    void walkingCreatureDealDamage(){
+
+        Creature creature = Creature.builder().aMaxHp(10).aCanFly(false).build();
+        ObstacleFactory obstacleFactory = new ObstacleFactory();
+        Obstacle obstacle = obstacleFactory.createObstacle("lava");
+
+        int hp = creature.getCurrentHp();
+        obstacle.apply(creature);
+
+        assertEquals(hp - 1, creature.getCurrentHp());
+
+    }
+
+    @Test
+    void flyingCreature(){
+
+        Creature creature = Creature.builder().aMaxHp(10).aCanFly(false).build();
+        ObstacleFactory obstacleFactory = new ObstacleFactory();
+        Obstacle obstacle = obstacleFactory.createObstacle("lava");
+
+        obstacle.apply(creature);
+
+        assertEquals(creature.getMaxHp(), creature.getCurrentHp());
+
+    }
+}
