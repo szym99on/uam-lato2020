655e5728cd42d2591cc620da7f729d46fe21d99f
Mikołaj Pokrywka <s444463@labs.wmi.amu.edu.pl>
2020-06-11 21:40:09 +0200

    working with states in spellbook

--- battle-gui/src/main/java/pl/psi/gui/SpellBookGui.java
+++ battle-gui/src/main/java/pl/psi/gui/SpellBookGui.java
@@ -4,6 +4,7 @@ import pl.psi.game.hero.converter.Hero;
 import pl.psi.game.spellbook.Spell;
 import pl.psi.game.spellbook.SpellBook;
 import pl.psi.game.spellbook.SummonUnits;
+import pl.psi.gui.states.NormalState;
 import pl.psi.gui.states.SummonUnitsState;
 import pl.psi.gui.states.StateMap;
 
@@ -11,14 +12,13 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.HashMap;
-import java.util.Map;
 
 public class SpellBookGui   {
 
     private SpellBook spellBook;
     private Hero hero;
-    private StateMap stateMap;
+    private StateMap stateMap = new NormalState();
+    private Spell spell;
 
     public SpellBookGui(Hero ahero, StateMap aStateMap){
         stateMap = aStateMap;
@@ -29,20 +29,16 @@ public class SpellBookGui   {
         frame.setSize(800, 600);
         frame.setLocation(50, 50);
         frame.setLayout(new GridLayout(15,6));
-
-        Map<String,JButton> buttonMap = new HashMap<String,JButton>();
-
         for(Spell item : spellBook.getHeroSpells()){
             JButton button = new JButton(item.getSpellName());
-            frame.add(new Button(item.getSpellName()));
+            frame.add(button);
             button.addActionListener(new ActionListener() {
                 @Override
                 public void actionPerformed(ActionEvent actionEvent) {
-
-                    stateMap = new SummonUnitsState();
-                    frame.setTitle("eloo");
-
-
+                   spell = item;
+                    if(spell instanceof SummonUnits){
+                        stateMap = new SummonUnitsState();
+                    }
                 }
             });
 
@@ -50,9 +46,16 @@ public class SpellBookGui   {
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
-
     }
 
+    public Spell getSelectedSpell(Spell aSpell) {
+        spell = aSpell;
 
+        return spell;
+    }
+
+    public StateMap getStateMap(){
+        return stateMap;
+    }
 
 }
