3ed4c1c76724ea71313faeb60abdf7357e49b671
SzmMar <marcelszmeterowicz@gmail.com>
2020-06-14 17:44:35 +0200

    updated converter request tests for artifacts with implemented function

--- battle-model/src/test/java/pl/psi/game/hero/artifacts/ConverterRequest.java
+++ battle-model/src/test/java/pl/psi/game/hero/artifacts/ConverterRequest.java
@@ -1,15 +1,24 @@
 package pl.psi.game.hero.artifacts;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import pl.psi.game.artifacts.Artifact;
 import pl.psi.game.artifacts.ArtifactFactory;
 import pl.psi.game.hero.converter.Hero;
+import pl.psi.game.spellbook.SpellBookInfoFactory;
+import pl.psi.game.spellbook.SpellFactory;
 
 import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ConverterRequest {
+    @BeforeAll
+    static void initializeFactories() {
+        new ArtifactsInfoFactory();
+        new ArtifactFactory();
+    }
+
     @Test
     void GetArtifactInfoFromFactory()
     {
@@ -23,15 +32,16 @@ public class ConverterRequest {
         artifact.apply(hero);
         assertEquals(hero.getKnowledge(),1);
     }
-    /*@Test
+    @Test
     void ArtifactCanChangeHeroStats() {
         Artifact artifact = ArtifactFactory.createArtifact(ArtifactsInfoFactory.getArtifact(ArtifactsInfoFactory.HELM_OF_THE_ALABASTER_UNICORN));
-        assertEquals(artifact.changeHeroStats(),true);
-    }*/
-    /*
+        assertEquals(artifact.affectsHeroStats(),true);
+    }
+
     @Test
     void ArtifactCantChangeHeroStats() {
-        Artifact artifact = ArtifactFactory.createArtifact(ArtifactsInfoFactory.getArtifact(ArtifactsInfoFactory.BREASTPLATE_OF_PETRIFIED_WOOD));
-        assertEquals(artifact.changeHeroStats(),false);
-    }*/
+//        Artifact artifact = ArtifactFactory.createArtifact(ArtifactsInfoFactory.getArtifact(ArtifactsInfoFactory.BREASTPLATE_OF_PETRIFIED_WOOD)); //breastplate is increasing hero's stats
+        Artifact artifact = ArtifactFactory.createArtifact(ArtifactsInfoFactory.getArtifact(ArtifactsInfoFactory.SURCOAT_OF_COUNTERPOISE));
+        assertEquals(artifact.affectsHeroStats(),false);
+    }
 }
