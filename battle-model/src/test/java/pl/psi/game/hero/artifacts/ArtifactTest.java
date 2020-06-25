58aea73e25eac26b0f6360797bff0023d332721c
Jakub Przybyla <kubaprzybya@gmail.com>
2020-06-25 20:01:25 +0200

    improve IncreaseMagicResistance test and introduce 'given - when - then' template for all tests

--- battle-model/src/test/java/pl/psi/game/hero/artifacts/ArtifactTest.java
+++ battle-model/src/test/java/pl/psi/game/hero/artifacts/ArtifactTest.java
@@ -27,45 +27,65 @@ public class ArtifactTest {
         new SpellBookInfoFactory();
     }
 
-//    ArtifactFactory tests
+//  ArtifactFactory tests
 
     @Test
     void factoryShouldCreateCreatureHealthArtifact() {
+        //given
         ArtifactInfo artifactInfo = ArtifactsInfoFactory.getArtifact(RING_OF_LIFE);
+
+        //when
         Artifact ringOfLife = ArtifactFactory.createArtifact(artifactInfo);
 
+        //then
         assertEquals(2, ringOfLife.getHealth());
     }
 
     @Test
     void factoryShouldCreateCreatureImmunityArtifact() {
+        //given
         ArtifactInfo artifactInfo = ArtifactsInfoFactory.getArtifact(PENDANT_OF_LIFE);
+
+        //when
         Artifact pendantOfLife = ArtifactFactory.createArtifact(artifactInfo);
 
+        //then
         assertEquals(LIGHTNING_BOLT, pendantOfLife.getSpellImmunityName());
     }
 
     @Test
     void factoryShouldCreateCreatureMagicResistanceArtifact() {
+        //given
         ArtifactInfo artifactInfo = ArtifactsInfoFactory.getArtifact(BOOTS_OF_POLARITY);
+
+        //when
         Artifact bootsOfPolarity = ArtifactFactory.createArtifact(artifactInfo);
 
+        //then
         assertEquals(15, bootsOfPolarity.getMagicResistance());
     }
 
     @Test
     void factoryShouldCreateCreatureSpeedArtifactBasedOnInfo() {
+        //given
         ArtifactInfo artifactInfo = ArtifactsInfoFactory.getArtifact(CAPE_OF_VELOCITY);
+
+        //when
         Artifact capeOfVelocity = ArtifactFactory.createArtifact(artifactInfo);
 
+        //then
         assertEquals(2, capeOfVelocity.getSpeed());
     }
 
     @Test
     void factoryShouldCreateHeroStatisticArtifactBasedOnInfo() {
+        //given
         ArtifactInfo artifactInfo = ArtifactsInfoFactory.getArtifact(SKULL_HELMET);
+
+        //when
         Artifact skullHelmet = ArtifactFactory.createArtifact(artifactInfo);
 
+        //then
         assertEquals(0, skullHelmet.getAttack());
         assertEquals(0, skullHelmet.getDefence());
         assertEquals(2, skullHelmet.getKnowledge());
@@ -74,143 +94,146 @@ public class ArtifactTest {
 
     @Test
     void factoryShouldCreateSpellDurationArtifact() {
+        //given
         ArtifactInfo artifactsInfo = ArtifactsInfoFactory.getArtifact(COLLAR_OF_CONJURING);
+
+        //when
         Artifact collarOfConjuring = ArtifactFactory.createArtifact(artifactsInfo);
 
+        //then
         assertEquals(1, collarOfConjuring.getDuration());
     }
 
-//    AffectsHeroStats test
-
-    @Test
-    void creatureBuffArtifactShouldReturnFalse() {
-        ArtifactInfo artifactInfo = ArtifactsInfoFactory.getArtifact(RING_OF_LIFE);
-        Artifact creatureBuffArtifact = ArtifactFactory.createArtifact(artifactInfo);
-
-        assertFalse(creatureBuffArtifact.affectsHeroStats());
-    }
-
-    @Test
-    void spellBuffArtifactShouldReturnFalse() {
-        ArtifactInfo artifactInfo = ArtifactsInfoFactory.getArtifact(RING_OF_CONJURING);
-        Artifact spellBuffArtifact = ArtifactFactory.createArtifact(artifactInfo);
-
-        assertFalse(spellBuffArtifact.affectsHeroStats());
-    }
-
-    @Test
-    void heroStatisticArtifactShouldReturnTrue() {
-        ArtifactInfo artifactInfo = ArtifactsInfoFactory.getArtifact(ARMOR_OF_WONDER);
-        Artifact heroStatisticArtifact = ArtifactFactory.createArtifact(artifactInfo);
-
-        assertTrue(heroStatisticArtifact.affectsHeroStats());
-    }
-
-
-//    CreatureHealthArtifact test
+//  CreatureHealthArtifact test
 
     @Test
     void artifactShouldIncreaseHealthBy2Points() {
+        //given
         ArtifactInfo artifactInfo = ArtifactsInfoFactory.getArtifact(RING_OF_LIFE);
         Artifact ringOfLife = ArtifactFactory.createArtifact(artifactInfo);
+
         List<Creature> creatures = new ArrayList<>();
         Creature creature = Creature.builder().aMaxHp(10).build();
         creatures.add(creature);
         Hero battleHero = Hero.builder().aCreatures(creatures).build();
 
+        //when
         try {
             ringOfLife.apply(battleHero);
         } catch (Exception e) {
             e.printStackTrace();
         }
 
+        //then
         assertEquals(12, battleHero.getCreatures().get(0).getMaxHp());
-        assertEquals(12, battleHero.getCreatures().get(0).getCurrentHp());
     }
 
 
-//    CreatureImmunityArtifact test
+//  CreatureImmunityArtifact test
 
     @Test
-    void artifactShouldGrantImmunityToCreature() {
+    void artifactShouldGrantImmunityToSpellToCreature() {
+        //given
         ArtifactInfo artifactInfo = ArtifactsInfoFactory.getArtifact(PENDANT_OF_LIFE);
         Artifact pendantOfDispassion = ArtifactFactory.createArtifact(artifactInfo);
+
         List<Creature> creatures = new ArrayList<>();
         Creature creature = Creature.builder().aMaxHp(10).build();
         creatures.add(creature);
         Hero battleHero = Hero.builder().aCreatures(creatures).build();
 
+        SpellFactory factory = new SpellFactory();
+        Spell spell = factory.createSpell(SpellBookInfoFactory.getSpell(SpellBookInfoFactory.LIGHTNING_BOLT));
+
+        //when
         try {
             pendantOfDispassion.apply(battleHero);
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-        SpellFactory factory = new SpellFactory();
-        Spell spell = factory.createSpell(SpellBookInfoFactory.getSpell(SpellBookInfoFactory.LIGHTNING_BOLT));
         spell.cast(creature);
 
-        assertEquals(true, creature.getMagicResistance().isImmuneToSpell(LIGHTNING_BOLT));
+        //then
+        assertTrue(creature.getMagicResistance().isImmuneToSpell(LIGHTNING_BOLT));
         assertEquals(10, creature.getCurrentHp());
     }
 
 
-//    CreatureMagicResistanceArtifact tests
+//  CreatureMagicResistanceArtifact tests
 
     @Test
     void artifactShouldIncreaseMagicResistanceBy15Percent() {
+        //given
         ArtifactInfo artifactInfo = ArtifactsInfoFactory.getArtifact(BOOTS_OF_POLARITY);
         Artifact bootsOfPolarity = ArtifactFactory.createArtifact(artifactInfo);
+
         List<Creature> creatures = new ArrayList<>();
-        Creature creature = Creature.builder().build();
-        creatures.add(creature);
+        Creature creature1 = Creature.builder().aMaxHp(1000).build();
+        Creature creature2 = Creature.builder().aMaxHp(1000).build();
+        creatures.add(creature1);
         Hero battleHero = Hero.builder().aCreatures(creatures).build();
 
+        //when
         try {
             bootsOfPolarity.apply(battleHero);
         } catch (Exception e) {
             e.printStackTrace();
         }
 
-        assertEquals(15, battleHero.getCreatures().get(0).getMagicResistance().getAllSpellsResistancePercentage());
+        SpellFactory factory = new SpellFactory();
+        Spell spell = factory.createSpell(SpellBookInfoFactory.getSpell(SpellBookInfoFactory.LIGHTNING_BOLT));
+        spell.cast(creature1);
+        spell.cast(creature2);
+
+        //then
+        assertEquals(15, creature1.getMagicResistance().getAllSpellsResistancePercentage());
+        assertEquals(971, creature1.getCurrentHp());
+        assertEquals(965, creature2.getCurrentHp());
     }
 
 
-//    CreatureSpeedArtifact tests
+//  CreatureSpeedArtifact tests
 
     @Test
     void artifactShouldIncreaseMoveRangeBy2Points() {
+        //given
         ArtifactInfo artifactInfo = ArtifactsInfoFactory.getArtifact(CAPE_OF_VELOCITY);
         Artifact capeOfVelocity = ArtifactFactory.createArtifact(artifactInfo);
+
         List<Creature> creatures = new ArrayList<>();
         Creature creature = Creature.builder().aMoveRange(2).build();
         creatures.add(creature);
         Hero battleHero = Hero.builder().aCreatures(creatures).build();
 
+        //when
         try {
             capeOfVelocity.apply(battleHero);
         } catch (Exception e) {
             e.printStackTrace();
         }
 
+        //then
         assertEquals(4, battleHero.getCreatures().get(0).getMoveRange());
     }
 
 
-//    HeroStatisticArtifact tests
+//  HeroStatisticArtifact tests
 
     @Test
     void artifactShouldIncreaseKnowledgeBy2Points() {
+        //given
         ArtifactInfo artifactInfo = ArtifactsInfoFactory.getArtifact(SKULL_HELMET);
         Artifact skullHelmet = ArtifactFactory.createArtifact(artifactInfo);
         Hero battleHero = Hero.builder().build();// default hero stats = 0
 
+        //when
         try {
             skullHelmet.apply(battleHero);
         } catch (Exception e) {
             e.printStackTrace();
         }
 
+        //then
         assertEquals(2, battleHero.getKnowledge());
         assertEquals(0, battleHero.getAttack());
         assertEquals(0, battleHero.getDefence());
@@ -219,16 +242,19 @@ public class ArtifactTest {
 
     @Test
     void artifactShouldIncreaseAttackBy2Points() {
+        //given
         ArtifactInfo artifactInfo = ArtifactsInfoFactory.getArtifact(CENTAURS_AX);
         Artifact centaursAx = ArtifactFactory.createArtifact(artifactInfo);
         Hero battleHero = Hero.builder().build();// default hero stats = 0
 
+        //when
         try {
             centaursAx.apply(battleHero);
         } catch (Exception e) {
             e.printStackTrace();
         }
 
+        //then
         assertEquals(0, battleHero.getKnowledge());
         assertEquals(2, battleHero.getAttack());
         assertEquals(0, battleHero.getDefence());
@@ -237,16 +263,19 @@ public class ArtifactTest {
 
     @Test
     void artifactShouldIncreaseDefenceBy2Points() {
+        //given
         ArtifactInfo artifactInfo = ArtifactsInfoFactory.getArtifact(SHIELD_OF_THE_DWARVEN_LORDS);
         Artifact shieldOfTheDwarvenLords = ArtifactFactory.createArtifact(artifactInfo);
         Hero battleHero = Hero.builder().build();// default hero stats = 0
 
+        //then
         try {
             shieldOfTheDwarvenLords.apply(battleHero);
         } catch (Exception e) {
             e.printStackTrace();
         }
 
+        //then
         assertEquals(0, battleHero.getKnowledge());
         assertEquals(0, battleHero.getAttack());
         assertEquals(2, battleHero.getDefence());
@@ -255,16 +284,19 @@ public class ArtifactTest {
 
     @Test
     void artifactShouldIncreasePowerBy2Points() {
+        //given
         ArtifactInfo artifactInfo = ArtifactsInfoFactory.getArtifact(MAGISTERS_SANDALS);
         Artifact magistersSandals = ArtifactFactory.createArtifact(artifactInfo);
         Hero battleHero = Hero.builder().build();// default hero stats = 0
 
+        //when
         try {
             magistersSandals.apply(battleHero);
         } catch (Exception e) {
             e.printStackTrace();
         }
 
+        //then
         assertEquals(0, battleHero.getKnowledge());
         assertEquals(0, battleHero.getAttack());
         assertEquals(0, battleHero.getDefence());
@@ -273,16 +305,19 @@ public class ArtifactTest {
 
     @Test
     void artifactShouldIncreaseAllStatsBy1Point() {
+        //given
         ArtifactInfo artifactInfo = ArtifactsInfoFactory.getArtifact(ARMOR_OF_WONDER);
         Artifact armorOfWonders = ArtifactFactory.createArtifact(artifactInfo);
         Hero battleHero = Hero.builder().build();// default hero stats = 0
 
+        //when
         try {
             armorOfWonders.apply(battleHero);
         } catch (Exception e) {
             e.printStackTrace();
         }
 
+        //then
         assertEquals(1, battleHero.getKnowledge());
         assertEquals(1, battleHero.getAttack());
         assertEquals(1, battleHero.getDefence());
@@ -290,24 +325,55 @@ public class ArtifactTest {
     }
 
 
-//    SpellDurationArtifact tests
+//  SpellDurationArtifact tests
 
     @Disabled // waiting for increaseDuration implementation by spells
     @Test
     void artifactShouldIncreaseSpellDurationBy2() {
+        //given
         ArtifactInfo artifactInfo = ArtifactsInfoFactory.getArtifact(RING_OF_CONJURING);
         Artifact ringOfConjuring = ArtifactFactory.createArtifact(artifactInfo);
+
         SpellInfo spellInfo = SpellBookInfoFactory.getSpell(HASTE);
         Hero battleHero = Hero.builder().build();
         battleHero.getSpellBook().createSpell(spellInfo);
 
+        //when
         try {
             ringOfConjuring.apply(battleHero);
         } catch (Exception e) {
             e.printStackTrace();
         }
 
+        //then
         assertEquals(3, battleHero.getSpellBook().getSpells().get(0).getDuration());
     }
+
+//  AffectsHeroStats test
+    
+    @Test
+    void creatureBuffArtifactShouldNotAffectHeroStats() {
+        ArtifactInfo artifactInfo = ArtifactsInfoFactory.getArtifact(RING_OF_LIFE);
+        Artifact creatureBuffArtifact = ArtifactFactory.createArtifact(artifactInfo);
+
+        assertFalse(creatureBuffArtifact.affectsHeroStats());
+    }
+
+    @Test
+    void spellBuffArtifactShouldNotAffectHeroStats() {
+        ArtifactInfo artifactInfo = ArtifactsInfoFactory.getArtifact(RING_OF_CONJURING);
+        Artifact spellBuffArtifact = ArtifactFactory.createArtifact(artifactInfo);
+
+        assertFalse(spellBuffArtifact.affectsHeroStats());
+    }
+
+    @Test
+    void heroStatisticArtifactShouldAffectHeroStats() {
+        ArtifactInfo artifactInfo = ArtifactsInfoFactory.getArtifact(ARMOR_OF_WONDER);
+        Artifact heroStatisticArtifact = ArtifactFactory.createArtifact(artifactInfo);
+
+        assertTrue(heroStatisticArtifact.affectsHeroStats());
+    }
+
 
 }
