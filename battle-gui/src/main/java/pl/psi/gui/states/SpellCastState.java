c64618e1bbfaaf845345f894b98ab02674ac2b7b
Mateusz Wawrzonek <matwaw4@st.amu.edu.pl>
2020-06-26 13:31:10 +0200

    Rename SummonUnitsState to SpellCastState

---
+++ battle-gui/src/main/java/pl/psi/gui/states/SpellCastState.java
@@ -0,0 +1,36 @@
+package pl.psi.gui.states;
+
+import pl.psi.game.GameEngine;
+import pl.psi.game.move.GuiTileIf;
+import pl.psi.game.spellbook.Spell;
+import pl.psi.gui.SpellBookGui;
+import pl.psi.gui.tiles.SummonUnitsPossibleTileFactoryDecorator;
+import pl.psi.gui.tiles.*;
+
+import java.awt.*;
+
+public class SummonUnitsState implements StateMap {
+    private GameEngine gameEngine;
+
+
+    @Override
+    public MapTile createTile(int aX, int aY, GameEngine aGameEngine) {
+        gameEngine = aGameEngine;
+
+        AbstractTileFactory factory = new DefaultTileFactory();
+        GuiTileIf somethingToRender = gameEngine.getByPoint(aX, aY);
+
+        if (somethingToRender != null) {
+            factory = new ObjectTileFactory(factory, somethingToRender);
+        }
+
+        if (gameEngine.getActiveCreature().getKey().equals(new Point(aX, aY))) {
+            factory = new ActiveObjectTileFactoryDecorator(factory);
+        }
+        if(gameEngine.spellCastPossible(aX,aY,SpellBookGui.selectedSpell))
+        factory = new SummonUnitsPossibleTileFactoryDecorator(factory, aX, aY, gameEngine);
+
+
+        return factory.generateTile();
+    }
+}
