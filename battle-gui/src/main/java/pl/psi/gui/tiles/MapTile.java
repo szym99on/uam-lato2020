6ca46ae904d4195e1af36c0b00e88dd25e6bd297
software-wizard <przemek@software-wizard.pl>
2020-06-27 06:37:39 +0200

    display icon units

--- battle-gui/src/main/java/pl/psi/gui/tiles/MapTile.java
+++ battle-gui/src/main/java/pl/psi/gui/tiles/MapTile.java
@@ -1,9 +1,13 @@
 package pl.psi.gui.tiles;
 
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
 
 public class MapTile extends StackPane {
 
@@ -11,14 +15,40 @@ public class MapTile extends StackPane {
     private final Label label;
     private String name;
 
+    public final static int TILE_HEIGHT = 60;
+    public final static int TILE_WIDTH = 60;
+
+
     MapTile(String aName) {
         name = aName;
-        rect = new Rectangle(60, 60);
+        rect = prepareRectangleBackground();
+        label = new Label(name);
+        getChildren().add(label);
+    }
+
+    private Rectangle prepareRectangleBackground() {
+        Rectangle rect;
+        rect = new Rectangle(TILE_WIDTH, TILE_HEIGHT);
         rect.setFill(Color.WHITE);
         rect.setStroke(Color.RED);
         getChildren().add(rect);
-        label = new Label(name);
-        getChildren().add(label);
+        return rect;
+    }
+
+    MapTile(Image aImage, String aCurrentState){
+        rect = prepareRectangleBackground();
+
+        Image image = aImage;
+        ImageView imageView = new ImageView(aImage);
+        imageView.setFitHeight(TILE_HEIGHT - 10);
+        imageView.setFitWidth(TILE_WIDTH);
+        label = new Label(aCurrentState);
+        label.setFont(new Font("Arial", 10));
+
+        VBox vbox = new VBox();
+        vbox.getChildren().add(imageView);
+        vbox.getChildren().add(label);
+        getChildren().add(vbox);
     }
 
     void setName(String aName){
--- battle-gui/src/main/java/pl/psi/gui/tiles/ObjectTileFactory.java
+++ battle-gui/src/main/java/pl/psi/gui/tiles/ObjectTileFactory.java
@@ -1,7 +1,12 @@
 package pl.psi.gui.tiles;
 
 
+import javafx.scene.image.Image;
+import pl.psi.game.fractions.Creature;
 import pl.psi.game.move.GuiTileIf;
+
+import javax.imageio.ImageIO;
+import java.io.IOException;
 
 public class ObjectTileFactory extends AbstractTileFactory {
 
@@ -9,11 +14,28 @@ public class ObjectTileFactory extends AbstractTileFactory {
 
     public ObjectTileFactory(AbstractTileFactory aDecorated, GuiTileIf aSomethingToRender) {
         super(aDecorated);
-        somethingToRender =aSomethingToRender;
+        somethingToRender = aSomethingToRender;
     }
 
     @Override
     public MapTile generateTile() {
+        MapTile ret;
+        if (somethingToRender.isCreature()){
+            try {
+                ret = new MapTile(new Image(ObjectTileFactory.class.getClassLoader().getResourceAsStream( ((Creature)somethingToRender).getName()+".png" )),"asdf");
+            } catch (Exception aE) {
+                ret = generateStringMapTile();
+            }
+        }
+        else
+        {
+            ret = generateStringMapTile();
+        }
+
+        return ret;
+    }
+
+    private MapTile generateStringMapTile() {
         MapTile ret = getDecorated().generateTile();
         ret.setName(somethingToRender.getDisplayName());
         return ret;
