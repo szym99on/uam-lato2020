3eae6096d9bd6dece375f05a608c4862d1613da0
Jakub Przybyla <kubaprzybya@gmail.com>
2020-07-01 15:26:41 +0200

    typo

--- battle-gui/src/main/java/pl/psi/gui/ArtifactInitializer.java
+++ battle-gui/src/main/java/pl/psi/gui/ArtifactInitializer.java
@@ -16,64 +16,27 @@ public class ArtifactInitializer extends AbstractInitializer {
         new ArtifactsInfoFactory();
     }
 
-    EconomyHero getH1() {
-        EconomyHero h1 = EconomyHero.builder().aGold(100000000).aHeroInfo(HeroInfoFactory.getHeroInfoByName(HeroInfoFactory.EDRIC)).build();
-        try {
-            //            ################################################################
-//            SCENARIO 1 - HeroStatisticArtifact
-//            ################################################################
-//            h1.buyCreature(FractionsInfoAbstractFactory.getFactory(FractionsInfoAbstractFactory.Fractions.RAMPART).getCreatureByTier(1), 100);
-//            h1.buyArtifact(ArtifactsInfoFactory.getArtifact(HELM_OF_THE_ALABASTER_UNICORN)); //+1 knowledge
-//            h1.buyArtifact(ArtifactsInfoFactory.getArtifact(MAGISTERS_SANDALS)); //+2 spell power
-//            h1.buyArtifact(ArtifactsInfoFactory.getArtifact(OGRES_CLUB_OF_HAVOC)); //+5 attack
-//            h1.buySpell(SpellBookInfoFactory.getSpell(MAGIC_ARROW));
-
-//            ################################################################
-//            SCENARIO 3 - SpellBuffArtifact (SpellDurationArtifact) (+3)
-//            ################################################################
-            h1.buyCreature(FractionsInfoAbstractFactory.getFactory(FractionsInfoAbstractFactory.Fractions.RAMPART).getCreatureByTier(3), 10);
-            h1.buyArtifact(ArtifactsInfoFactory.getArtifact(RING_OF_CONJURING)); //+3
-            h1.buySpell(SpellBookInfoFactory.getSpell(HASTE));
-
-
-
-        } catch (Exception aE) {
-            aE.printStackTrace();
-        }
-        return h1;
-    }
-
-    EconomyHero getH2() {
-        EconomyHero h2 = EconomyHero.builder().aGold(100000000).aHeroInfo(HeroInfoFactory.getHeroInfoByName(HeroInfoFactory.EDRIC)).build();
-        try {
-//            ################################################################
-//            SCENARIO 1
-//            ################################################################
-//            h2.buyCreature(FractionsInfoAbstractFactory.getFactory(FractionsInfoAbstractFactory.Fractions.RAMPART).getCreatureByTier(1), 100);
-//            h2.buySpell(SpellBookInfoFactory.getSpell(MAGIC_ARROW));
-
-//            ################################################################
-//            SCENARIO 3
-//            ################################################################
-            h2.buyCreature(FractionsInfoAbstractFactory.getFactory(FractionsInfoAbstractFactory.Fractions.RAMPART).getCreatureByTier(3),10);
-            h2.buySpell(SpellBookInfoFactory.getSpell(HASTE));
-
-        } catch (Exception aE) {
-            aE.printStackTrace();
-        }
-        return h2;
-    }
-
 //    EconomyHero getH1() {
-//        EconomyHero h1 = EconomyHero.builder().aGold(100000000).aHeroInfo(HeroInfoFactory.getHeroInfoByName(HeroInfoFactory.BRISSA)).build();
+//        EconomyHero h1 = EconomyHero.builder().aGold(100000000).aHeroInfo(HeroInfoFactory.getHeroInfoByName(HeroInfoFactory.EDRIC)).build();
 //        try {
+//            //            ################################################################
+////            SCENARIO 1 - HeroStatisticArtifact
 ////            ################################################################
-////            SCENARIO 2 - CreatureBuffArtifact
+////            h1.buyCreature(FractionsInfoAbstractFactory.getFactory(FractionsInfoAbstractFactory.Fractions.RAMPART).getCreatureByTier(1), 100);
+////            h1.buyArtifact(ArtifactsInfoFactory.getArtifact(HELM_OF_THE_ALABASTER_UNICORN)); //+1 knowledge
+////            h1.buyArtifact(ArtifactsInfoFactory.getArtifact(MAGISTERS_SANDALS)); //+2 spell power
+////            h1.buyArtifact(ArtifactsInfoFactory.getArtifact(OGRES_CLUB_OF_HAVOC)); //+5 attack
+////            h1.buySpell(SpellBookInfoFactory.getSpell(MAGIC_ARROW));
+//
 ////            ################################################################
-//            h1.buyCreature(FractionsInfoAbstractFactory.getFactory(FractionsInfoAbstractFactory.Fractions.STRONGHOLD).getCreatureByTier(1), 20);
-//            h1.buyArtifact(ArtifactsInfoFactory.getArtifact(PENDANT_OF_LIFE)); //to LIGHTNING_BOLT spell
-//            h1.buySpell(SpellBookInfoFactory.getSpell(MAGIC_ARROW));
-//            h1.buySpell(SpellBookInfoFactory.getSpell(LIGHTNING_BOLT));
+////            SCENARIO 3 - SpellBuffArtifact (SpellDurationArtifact) (+3)
+////            ################################################################
+////            h1.buyCreature(FractionsInfoAbstractFactory.getFactory(FractionsInfoAbstractFactory.Fractions.RAMPART).getCreatureByTier(3), 10);
+////            h1.buyArtifact(ArtifactsInfoFactory.getArtifact(RING_OF_CONJURING)); //+3
+////            h1.buySpell(SpellBookInfoFactory.getSpell(HASTE));
+//
+//
+//
 //        } catch (Exception aE) {
 //            aE.printStackTrace();
 //        }
@@ -81,20 +44,58 @@ public class ArtifactInitializer extends AbstractInitializer {
 //    }
 //
 //    EconomyHero getH2() {
-//        EconomyHero h2 = EconomyHero.builder().aGold(100000000).aHeroInfo(HeroInfoFactory.getHeroInfoByName(HeroInfoFactory.BRISSA)).build();
+//        EconomyHero h2 = EconomyHero.builder().aGold(100000000).aHeroInfo(HeroInfoFactory.getHeroInfoByName(HeroInfoFactory.EDRIC)).build();
 //        try {
 ////            ################################################################
-////            SCENARIO 2
+////            SCENARIO 1
 ////            ################################################################
-//            h2.buyCreature(FractionsInfoAbstractFactory.getFactory(FractionsInfoAbstractFactory.Fractions.STRONGHOLD).getCreatureByTier(1),20);
-//            h2.buyArtifact(ArtifactsInfoFactory.getArtifact(RECANTERS_CLOAK)); //+20% resistance
+//            h2.buyCreature(FractionsInfoAbstractFactory.getFactory(FractionsInfoAbstractFactory.Fractions.RAMPART).getCreatureByTier(1), 100);
 //            h2.buySpell(SpellBookInfoFactory.getSpell(MAGIC_ARROW));
-//            h2.buySpell(SpellBookInfoFactory.getSpell(LIGHTNING_BOLT));
+//
+////            ################################################################
+////            SCENARIO 3
+////            ################################################################
+////            h2.buyCreature(FractionsInfoAbstractFactory.getFactory(FractionsInfoAbstractFactory.Fractions.RAMPART).getCreatureByTier(3),10);
+////            h2.buySpell(SpellBookInfoFactory.getSpell(HASTE));
 //
 //        } catch (Exception aE) {
+//            aE.printStackTrace();
 //        }
 //        return h2;
 //    }
+
+    EconomyHero getH1() {
+        EconomyHero h1 = EconomyHero.builder().aGold(100000000).aHeroInfo(HeroInfoFactory.getHeroInfoByName(HeroInfoFactory.BRISSA)).build();
+        try {
+//            ################################################################
+//            SCENARIO 2 - CreatureBuffArtifact
+//            ################################################################
+            h1.buyCreature(FractionsInfoAbstractFactory.getFactory(FractionsInfoAbstractFactory.Fractions.STRONGHOLD).getCreatureByTier(1), 20);
+            h1.buyArtifact(ArtifactsInfoFactory.getArtifact(PENDANT_OF_LIFE)); //to LIGHTNING_BOLT spell
+            h1.buyArtifact(ArtifactsInfoFactory.getArtifact(CAPE_OF_VELOCITY)); //+2 speed
+            h1.buySpell(SpellBookInfoFactory.getSpell(MAGIC_ARROW));
+            h1.buySpell(SpellBookInfoFactory.getSpell(LIGHTNING_BOLT));
+        } catch (Exception aE) {
+            aE.printStackTrace();
+        }
+        return h1;
+    }
+
+    EconomyHero getH2() {
+        EconomyHero h2 = EconomyHero.builder().aGold(100000000).aHeroInfo(HeroInfoFactory.getHeroInfoByName(HeroInfoFactory.BRISSA)).build();
+        try {
+//            ################################################################
+//            SCENARIO 2
+//            ################################################################
+            h2.buyCreature(FractionsInfoAbstractFactory.getFactory(FractionsInfoAbstractFactory.Fractions.STRONGHOLD).getCreatureByTier(1),20);
+            h2.buyArtifact(ArtifactsInfoFactory.getArtifact(RECANTERS_CLOAK)); //+20% resistance
+            h2.buySpell(SpellBookInfoFactory.getSpell(MAGIC_ARROW));
+            h2.buySpell(SpellBookInfoFactory.getSpell(LIGHTNING_BOLT));
+
+        } catch (Exception aE) {
+        }
+        return h2;
+    }
 
 
 }
